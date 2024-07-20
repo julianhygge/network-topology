bea432e3c9063c7cb5a6a91dc910cd911153103a
Julian Arroyave <87373005+julianhygge@users.noreply.github.com>
2024-07-20 20:26:33 +0530

    Load profile schema

---
+++ app/data/schemas/load_profile/load_profile_schema.py
@@ -0,0 +1,38 @@
+from peewee import ForeignKeyField, CharField, BooleanField, IntegerField, DateTimeField, DoubleField
+from app.data.schemas.auth.auditable_base import AuditableBase
+from app.data.schemas.schema_base import BaseModel
+from app.data.schemas.transactional.user_schema import User
+
+
+class LoadProfileAuditableBase(AuditableBase):
+    active = BooleanField()
+
+    class Meta:
+        schema = 'load'
+        abstract = True
+
+
+class LoadProfileBase(BaseModel):
+    class Meta:
+        schema = 'load'
+        abstract = True
+
+
+class LoadProfiles(LoadProfileAuditableBase):
+    load_profile_id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(User, backref='load_profiles')
+    profile_name = CharField(max_length=50)
+    public = BooleanField()
+
+    class Meta:
+        table_name = 'load_profiles'
+
+
+class LoadProfileDetails(LoadProfileBase):
+    profile_detail_id = IntegerField(primary_key=True)
+    profile_id = ForeignKeyField(LoadProfiles, backref='details')
+    timestamp = DateTimeField()
+    consumption_kwh = DoubleField()
+
+    class Meta:
+        table_name = 'load_profile_details'
