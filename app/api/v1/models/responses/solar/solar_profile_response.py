b826cc39c8e09e0027e4a0a92c6fe10f15cae0ee
hhimanshu123 <118249281+hhimanshu123@users.noreply.github.com>
2024-08-31 22:59:37 +0530

    update-sola-profile

--- app/api/v1/models/responses/solar/solar_profile_response.py
+++ app/api/v1/models/responses/solar/solar_profile_response.py
@@ -7,13 +7,13 @@ from app.api.v1.models.requests.solar.solar_profile_request import TiltType
 
 
 class SolarProfileResponse(BaseModel):
-    max_capacity_kw: Optional[Decimal] = None
     solar_available: bool
     house_id: UUID4
     installed_capacity_kw: Optional[Decimal]
-    tilt_type: Optional[TiltType]
+    tilt_type: TiltType
     years_since_installation: Optional[Decimal]
     available_space_sqft: Optional[Decimal]
+    simulated_available_space_sqft: Optional[Decimal]
     simulate_using_different_capacity: bool
     capacity_for_simulation_kw: Optional[Decimal]
 
