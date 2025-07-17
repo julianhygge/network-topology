7cc08c2ae19772e3258188d5ccd9e4e0bf5b39cb
julianhygge <julian@hygge.energy>
2025-07-17 19:13:28 +0530

    Uncomment simulation_algorithm_type_id

--- app/data/schemas/simulation/simulation_runs_schema.py
+++ app/data/schemas/simulation/simulation_runs_schema.py
@@ -15,8 +15,10 @@ from app.data.schemas.master.master_schema import (
     SimulationAlgorithm,
 )
 from app.data.schemas.schema_base import BaseModel
-from app.data.schemas.simulation.simulation_container_schema import SimulationContainer
-from app.data.schemas.transactional.topology_schema import Node, Locality
+from app.data.schemas.simulation.simulation_container_schema import (
+    SimulationContainer,
+)
+from app.data.schemas.transactional.topology_schema import Locality, Node
 from app.data.schemas.transactional.user_schema import User
 
 
@@ -26,10 +28,11 @@ class SimulationRuns(BaseModel):
     topology_root_node_id = ForeignKeyField(
         Node, backref="simulation", column_name="topology_root_node_id"
     )
-    # simulation_algorithm_type_id = ForeignKeyField(
-    #     SimulationAlgorithm,
-    #     backref="simulation_algo",
-    #     column_name="simulation_algorithm_type_id")
+    simulation_algorithm_type_id = ForeignKeyField(
+        SimulationAlgorithm,
+        backref="simulation_algo",
+        column_name="simulation_algorithm_type_id",
+    )
     simulation_algorithm_type_id = UUIDField()
     billing_cycle_month = IntegerField()
     billing_cycle_year = IntegerField()
@@ -46,14 +49,12 @@ class SimulationRuns(BaseModel):
     simulation_start_timestamp = DateTimeField()
     simulation_end_timestamp = DateTimeField()
     locality_id = ForeignKeyField(
-        Locality,
-        backref="simulation",
-        column_name="locality_id"
+        Locality, backref="simulation", column_name="locality_id"
     )
     simulation_container_id = ForeignKeyField(
         SimulationContainer,
         on_delete="CASCADE",
-        column_name="simulation_container_id"
+        column_name="simulation_container_id",
     )
     description = CharField()
     run_sequence_identifier = CharField()
